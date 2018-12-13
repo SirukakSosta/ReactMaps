a28d91161c4c930df511b20bfbf9057026d110cf
ATC\tkakouris <t.kakouris@atc.gr>
2018-12-13 11:48:23 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/** @format */
+import React from 'react';
+import { AppRegistry, View} from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+import AnimatedMarkers from './src/components/AnimatedMarkers';
+const App = () => {
+    return (
+        <View>
+            <Header headerText={'Albums'} />
+            <AlbumList />
+            <AnimatedMarkers />
+        </View>
+    );
+};
+
+AppRegistry.registerComponent('albums', () => App);
